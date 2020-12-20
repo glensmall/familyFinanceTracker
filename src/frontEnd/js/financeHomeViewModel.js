6eae9d96a6060ea4d407e4f51e21f24ec421e3dc
Glen Small <14293295+glensmall@users.noreply.github.com>
2020-12-20 21:37:25 +0000

    trying to get knockout to work

--- src/frontEnd/js/financeHomeViewModel.js
+++ src/frontEnd/js/financeHomeViewModel.js
@@ -1,20 +1,32 @@
 (function(){
 
+function Transaction(_description, _date, _amount, _balance){
+    var self = this;
+    self.Description = _description
+    self.Date = _date
+    self.Amount = _amount
+    self.Balance = _balance
+}
+
 // finfanceHomeViewModel
 function financeHomeViewModel(){
 
     var self = this;
 
-    // will hold the transactions from the backend
-    self.transactionList = ko.observable();
-
-
-    // we're going to need to make a call to our backend and get the data
-    $.get('/getEntries/', {Date: range}, self.transactionList);
-
-}
-
+    self.transactionList = ko.observableArray([]);
+ 
+    $.ajax({
+        method : 'GET',
+        url : '/backendEngine/getEntries/',
+        dataType : 'json',
+        success : function(data){
+            for(var entry in data) {
+                transactionList.push(new Transaction(entry[0], entry[1], entry[2], entry[3]))
+            }
+        }
+    });
 
 // apply this view model to the items in the page and go for it
 ko.applyBindings(new financeHomeViewModel());
+}
 })();

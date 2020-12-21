6555c93791119cd56695dc73905dc81b050fa585
Glen Small <14293295+glensmall@users.noreply.github.com>
2020-12-21 16:47:54 +0000

    getting Kockout to work with test data

--- src/frontEnd/js/financeHomeViewModel.js
+++ src/frontEnd/js/financeHomeViewModel.js
@@ -1,32 +1,29 @@
-(function(){
-
-function Transaction(_description, _date, _amount, _balance){
+function TransactionEntry(data){
     var self = this;
-    self.Description = _description
-    self.Date = _date
-    self.Amount = _amount
-    self.Balance = _balance
+
+    self.details = ko.observable(data);
 }
 
 // finfanceHomeViewModel
 function financeHomeViewModel(){
 
-    var self = this;
+    self = this
 
-    self.transactionList = ko.observableArray([]);
- 
-    $.ajax({
+    self.Testing = "This is some Knockout Test Data";
+    self.userTransactions = ko.observableArray();
+
+   $.ajax({
         method : 'GET',
-        url : '/backendEngine/getEntries/',
-        dataType : 'json',
-        success : function(data){
-            for(var entry in data) {
-                transactionList.push(new Transaction(entry[0], entry[1], entry[2], entry[3]))
+        url : 'http://localhost:8000/testdata.json',
+        dataType : 'json'
+    }).then(function(result){
+            for(index in result.Transactions){
+                self.userTransactions.push(new TransactionEntry(result.Transactions[index]));
             }
-        }
-    });
+        });
 
+    }
+    
 // apply this view model to the items in the page and go for it
 ko.applyBindings(new financeHomeViewModel());
-}
-})();
+
